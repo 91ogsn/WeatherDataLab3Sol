eb77a05b77b6938af9abc25ca8ada3dfea94fbe0
91ogsn <stefan.ostling@hotmail.com>
2025-11-27 11:11:16 +0100

    börjat fixa i program.cs

--- WeatherDataLab3/Program.cs
+++ WeatherDataLab3/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherDataLab3.DataAcces;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var filePath = "../../../TempFuktData.csv";
+
+        try
+        {
+            InitializeData(filePath);
+
+            RunApplication();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Något gick fel, vi avbryter denna sändning...");
+            Console.WriteLine(ex);
+            Console.ReadKey();
+        }
+               
+    }
+
+    
+    // ==== Metoder ==== \\
+    private static void InitializeData(string csvPath)
+    {
+        Console.WriteLine("Initierar databas...");
+
+        using var db = new WeatherDBContext();
+
+        // Applicera migrations skapar databasen om den inte finns
+        db.Database.Migrate();
+
+        // Läs in CSV endast om tabellen är tom
+        if (!db.WeatherRecords.Any())
+        {
+            Console.WriteLine("Databasen är tom. Importerar CSV...");
+            //CsvWeatherImporter.Import(db, csvPath);
+        }
+
+        Console.WriteLine("Databasen är redo.");
+    }
+
+    private static void RunApplication()
+    {
+        Console.WriteLine("Fixa! Ska starta meny");
+        Console.ReadKey();
+    }
+}
